0a331166b89f93c780c166be7b964404569f6d8e
Bunburry <gintariukass@gmail.com>
2018-01-21 18:36:32 +0000

    Update connect_database.php

--- Mysql_local/connect_database.php
+++ Mysql_local/connect_database.php
@@ -6,14 +6,9 @@
 <?php
 
 DEFINE ('DB_USER','root');
-DEFINE ('DB_PSWD','pomidoras');
+DEFINE ('DB_PSWD','****');
 DEFINE ('DB_HOST','localhost');
-
-/*DEFINE ('DB_USER','jobdb');
-DEFINE ('DB_PSWD','cAn_3H07v');
-DEFINE ('DB_HOST','direct.107.161.31.122');*/
 DEFINE ('DB_NAME','netscapes');
-//DEFINE ('DB_PORT','12322');
 
 // creates connection
 $dbcon = mysqli_connect(DB_HOST, DB_USER, DB_PSWD, DB_NAME);
